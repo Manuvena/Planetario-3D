8fa9d28d2c1b6bc23fe823df1c850b704e037a05
Manuel Venarotta <manuel411@libero.it>
2026-05-24 18:20:23 +0200

    Aggiornato main.cpp: Codice di Tappa 02 ottimizzato e commentato

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
     sf::ContextSettings settings; //oggetto che viene mandato a GPU che serve a definire le proprietà HW da attivare
     settings.depthBits= 24; //z-buffer, 24b per ogni px per calcolare la distanza degli oggetti da telecamera
     settings.stencilBits= 8;
-    settings.antiAliasingLevel= 4;
+    settings.antiAliasingLevel= 4; //tecnica MSAA per evitare aliasing
     //per impostare versione principale e secondaria di OpenGL
     settings.majorVersion= 4;
     settings.minorVersion= 1;
@@ -16,7 +16,7 @@ int main() {
 
     //istanzia oggetto window e chiede a S.O. di aprire finestra 800x600 px, con barra titolo e bordi, passando struttura settings
     sf::Window window(sf::VideoMode({800, 600}), "Planetario 3D - Tappa 02", sf::State::Windowed, settings);
-    window.setVerticalSyncEnabled(true);
+    window.setVerticalSyncEnabled(true); //abilita il V-Sync per far si hce programma segua il refresh rate del monitor
 
     //getFunction serve a chiedere a S.O. indirizzo di memoria driver GPU, GLAD li prende e collega al codice
     if(!gladLoadGL(reinterpret_cast<GLADloadfunc>(sf::Context::getFunction))) {
@@ -25,6 +25,7 @@ int main() {
     }
 
     glClearColor(0.05f, 0.05f, 0.15f, 1.0f); //modifichiamo registro di memoria della GPU cambiando il colore di pulizia schermo
+    glEnable(GL_DEPTH_TEST); //abilitiamo lo z-buffer
 
     while(window.isOpen()) {
         while(const std::optional<sf::Event> event= window.pollEvent()) {
